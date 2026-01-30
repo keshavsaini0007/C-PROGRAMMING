67b2ba6b5a9c0d823df2b757183635d986437581
keshav saini <sainikeshav241@gmail.com>
2026-01-30 09:04:43 +0530

    push

---
+++ basic/basics of variable/surface_area_of_sphere.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+int main ()
+{
+ float s;
+ float r ;
+ r = 67;
+ s = (4*3.1415*r*r);
+ printf("if the radius of sphare is :-\n" );
+ printf("%f",r);
+ printf(" units.");
+  printf("\n");
+ printf("then surface area of sphere is :-\n");
+ printf("%f",s);
+ printf(" square units.");
+  return 0;
+
+}
