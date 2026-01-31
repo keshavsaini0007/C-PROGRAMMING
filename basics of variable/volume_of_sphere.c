6476ae9435b5e6af8ef731e485d233b47c367efd
keshav saini <sainikeshav241@gmail.com>
2026-01-31 14:46:40 +0530

    push

---
+++ basics of variable/volume_of_sphere.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+ float v;
+ float r;
+ float pi = 3.1415;
+ r = 120;
+ printf("if the radius of sphere is ");
+ printf("%f",r);
+ v = (pi*4*r*r*r)/3;
+ printf("\nthen volume of sphere is \n");
++printf("%f",v);
+ 
+    return 0;
+}
+
