90ca7e5faf00a65bffaa6fe0d6d99c86deb35abc
keshav saini <sainikeshav241@gmail.com>
2026-05-15 22:10:34 +0530

    includes string

---
+++ string/include_string.c
@@ -0,0 +1,39 @@
+// method 1....
+
+// # include <stdio.h>
+// # include <string.h>
+// int main(){
+
+//     char str[555];
+//     scanf("%[^\n]s",str);
+//     printf("%s",str);
+//     return 0;
+// }
+
+
+
+// method 2.....
+
+
+
+
+
+
+// # include <stdio.h>
+// # include <string.h>
+// int main(){
+
+//     char str[555];
+
+//     gets(str);
+
+//     puts(str);
+//     return 0;
+// }
+
+
+
+
+
+
+
