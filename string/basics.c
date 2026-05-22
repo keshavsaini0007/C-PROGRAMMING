1e05dc102f060b76e354f7c0f3b4cae998a7a659
keshav saini <sainikeshav241@gmail.com>
2026-05-22 22:39:52 +0530

    Create basics.c

---
+++ string/basics.c
@@ -0,0 +1,22 @@
+// ascii values of the alphabets
+# include <stdio.h>
+int main (){
+    char ch[4] = {'A','B','C','D'};
+    // printf("%c->",ch[0]);
+    // printf("%d\n",ch[0]);
+
+    // printf("%c->",ch[1]);
+    // printf("%d\n",ch[1]);
+
+
+    // printf("%c->",ch[2]);
+    // printf("%d\n",ch[2]);
+
+    // printf("%c->",ch[3]);
+    // printf("%d\n",ch[3]);
+
+
+    for(int i=0; i<=3; i++){  printf("%d\n",ch[i]); }
+    
+    return 0;
+} 
