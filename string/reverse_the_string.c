efcf57314783b13a7cfacab0053158f743a902e6
keshav saini <sainikeshav241@gmail.com>
2026-06-04 07:50:30 +0530

    rev the string

---
+++ string/reverse_the_string.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <string.h>
+
+int main (){
+    char str[999];
+    puts("enter your text : ");
+    gets(str);
+
+    
+
+    puts("the reversed text was  : ");
+
+    
+    puts(str);
+    return 0;
+}
