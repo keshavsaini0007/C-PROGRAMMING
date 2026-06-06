5e7ef37d0e5069ceaf179d3a5f16bfffc1e78c51
keshav saini <sainikeshav241@gmail.com>
2026-06-06 21:40:00 +0530

    Create xyz.c

---
+++ basics of variable/xyz.c
@@ -0,0 +1,34 @@
+// # include <stdio.h>
+// int main ()
+// {
+//     char str[] = "keshav saini"   ;
+//     int i = 0;
+//     while(str[i]!='\0'){
+//         printf("%c",str[i]);
+//         i++;
+//     }
+
+
+//     return 0;
+// }
+
+
+
+
+# include <stdio.h>
+int main (){
+    char str[] = "keshav saini keshav saini ";
+    int i = 0;
+    while (str[i]!='\0'){
+           i++; }
+    printf("%d",i);
+    return 0;
+}
+
+
+
+
+
+
+
+
