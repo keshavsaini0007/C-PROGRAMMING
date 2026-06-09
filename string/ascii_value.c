6bc0435558cd6965f68163533f40297fc59936a5
keshav saini <sainikeshav241@gmail.com>
2026-06-09 12:31:47 +0530

    ascii

---
+++ string/ascii_value.c
@@ -0,0 +1,38 @@
+// (int)ch -> means ascii value of character stored in ch
+
+
+
+
+// # include <stdio.h>
+// int main ()
+// {
+//     char ch = '2';
+//     int x = (int)ch;
+//     printf("%d ",x);
+
+//     return 0;
+// }
+
+# include <stdio.h>
+int main ()
+{
+    char ch;
+    printf("enter the character to find ascii value : ");
+    scanf("%c",&ch);
+
+    
+    int x = (int)ch;
+    printf("%d ",x);
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
